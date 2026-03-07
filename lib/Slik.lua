b15e14a21c71dcbb0ca3253b3c2d0f3e1cb3c18b
Filipe Junqueiro <filipe.junqueiro.2005@gmail.com>
2026-03-07 20:38:33 +0000

    ref(lib): Drop callback hell support :D

--- lib/Slik.lua
+++ lib/Slik.lua
@@ -3,50 +3,25 @@
 ---@field isSuccess boolean
 ---@field error string
 
-local Promise = promise
 local Await = Citizen.Await
 local ResourceName = GetCurrentResourceName()
-
----@param fn function
----@param query string
----@param parameters table
----@return Result
-local function await(fn, query, parameters)
-    local promise = Promise.new()
-
-    fn(query, parameters, function(result, error)
-        if error then
-            return promise:reject(error)
-        end
-
-        promise:resolve(result)
-    end, ResourceName, true)
-
-    return Await(promise)
-end
 
 local Silk = {}
 local ESilk = exports.silk
 
 for _, method in pairs({
-    "query",
+    "execute",
     "single",
     "insert",
     "update",
 }) do
-    Silk[method] = setmetatable(
-        {
-            method = method,
-            await = function(query, parameters)
-                return await(ESilk[method], query, parameters)
-            end
-        },
-        {
-            __call = function(self, query, parameters, callback)
-                return ESilk[self.method](nil, query, parameters, callback)
-            end
-        }
-    )
+    Silk[method] = function(query, parameters)
+        return Await(ESilk[method](nil, {
+            query = query,
+            parameters = parameters,
+            invokingResource = ResourceName
+        }))
+    end
 end
 
 _ENV.Silk = Silk
